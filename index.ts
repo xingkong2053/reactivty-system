c9d6aa41fed1971636e95ea95bda979fd2268358
penghui <penghui_2053@163.com>
2022-06-23 22:47:43 +0800

    解决计算属性内部所依赖的响应式数据变化时无法触发外部的effectFn的bug

--- index.ts
+++ index.ts
@@ -85,21 +85,39 @@ function computed<T>(getter: ()=>T){
       // 每次getter中的依赖项改变时, 都会执行trigger, 进而执行scheduler,
       // 那么将dirty变为true的工作就写在这里就行
       dirty = true
+      // 触发obj.value所依赖的effectFn
+      trigger(obj, "value")
     }
   })
-  return {
+
+
+  const obj = {
     // 当读取到.value时才去执行effectFn()
     get value(){
       if(dirty){
         val = effectFn() as T;
         dirty = false;
       }
+      // 当在一个副作用函数时调用computed时, 如
+      // effect(()=>{
+      //   const bar = computed(()=>obj.cnt + obj.foo)
+      //   console.log(bar.value)
+      // })
+      // 我们希望这个副作用函数会成为obj.cnt和obj.foo的依赖,
+      // 但是现实并不会如此, 以为上边的代码本质上就是effect函数嵌套
+      // 在读取内层obj的值时, 并不会把外层的effect作为其依赖
+      // 解决方法: 
+      // 当读取计算属性的值时, 调用trace追踪value
+      // 当value改变时(这里是dirty = true), 调用trigger触发响应
+      // 这是的activeEffect是外层的effectFn
+      track(obj, "value")
       return val;
     },
     set value(val){
       console.error("计算属性不允许set");
     }
   }
+  return obj;
 }
 
 const obj = new Proxy(data, {
