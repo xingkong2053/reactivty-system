90370a41088ab2cdb397d6bbc8b34d1053cf4c1c
penghui <penghui_2053@163.com>
2022-06-26 09:29:53 +0800

    使用Reflect.*  方法来解决访问器属性中this问题

--- effect.ts
+++ effect.ts
@@ -41,13 +41,28 @@ export function effect(fn: () => unknown, options: EffectOptions = {}) {
 
 export function reactive(data: Record<string | symbol, any>){
   return new Proxy(data, {
-    get(target, key) {
+    get(target, key, receiver) {
       track(target, key)
-      return target[key]
+      // 使用target[key]会带来的问题, 如:
+      // let obj = {
+      //   foo: 1,
+      //   get bar(){
+      //     return this.foo
+      //   }
+      // }
+      // let p = reactive(obj)
+      // 在上例中我们希望在追踪bar的响应式的时候也会追踪foo,
+      // 然而使用target[key]的方式行不通
+      // 原因是在执行bar的getter函数时, getter函数里面的this指向的是原对象obj而不是代理对象p
+      // 所以我们在获取对象属性时, 要修改这个属性getter函数内部的this值
+      // 这就要使用Reflect.get()
+      // return target[key]
+      return Reflect.get(target, key, receiver /* 指向创建后的代理对象 */)
     },
-    set(target, key, newVal) {
-      target[key] = newVal
-      trigger(target, key)
+    set(target, key, newVal, receiver) {
+      // target[key] = newVal
+      Reflect.set(target, key, newVal, receiver)
+      trigger(target, key) 
       return true
     }
   })
